c1d3172cfcf50db95f5ff99b8381e6cd07c9dea2
dahibe <dahia810@gmail.com>
2018-08-30 20:29:12 -0400

    Fixes por la URL

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
     #app
-      img(src='https://dahibe.github.io/platzimusic/')
+      img(src='https://dahibe.github.io/platzimusic/dist/logo.png')
       h1 PlatziMusic
       select(v-model="selectedCountry")
         option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
