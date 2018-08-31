8246d0a9e7c9576b41232d0971012f3709768b43
dahibe <dahia810@gmail.com>
2018-08-30 20:07:12 -0400

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+  appName: 'platzimusic',
+  apiKey: '0fafb928461fb29625c31b34042a9515',
+  secret: '2273a2804344e67a26a7cc3b2b82df5e',
+  registeredTo: 'dbyte227'
+}
+
+// Nombre de aplicación	platzimusic
+// Clave API	0fafb928461fb29625c31b34042a9515
+// Secreto compartido	2273a2804344e67a26a7cc3b2b82df5e
+// Registrado para	dbyte227
